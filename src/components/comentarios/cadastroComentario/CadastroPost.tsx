59a0ee34d73d6e3f5f6c9c5c8050ff0c5c442281
Gustavo Rocha <111547125+gust-tech@users.noreply.github.com>
2023-03-29 18:43:25 -0300

    Comentários - in dev

---
+++ src/components/comentarios/cadastroComentario/CadastroPost.tsx
@@ -0,0 +1,232 @@
+import React, { ChangeEvent, useEffect, useState } from 'react'
+import { Container, Typography, TextField, Button, Select, InputLabel, MenuItem, FormControl, FormHelperText, Grid } from "@material-ui/core";
+import './CadastroPost.css';
+import { useNavigate, useParams } from 'react-router-dom';
+import Tema from '../../../models/Tema';
+import Postagem from '../../../models/Postagem';
+import { busca, buscaId, post, put } from '../../../services/Service';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import { toast } from 'react-toastify';
+import User from '../../../models/User';
+
+function CadastroPost() {
+    let navigate = useNavigate();
+    const { id } = useParams<{ id: string }>();
+    const [temas, setTemas] = useState<Tema[]>([])
+    const [users, setUsers] = useState<User[]>([])
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+      );
+
+    useEffect(() => {
+        if (token == "") {
+            toast.error('Você precisa estar logado', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
+            navigate("/login")
+
+        }
+    }, [token])
+
+    const [tema, setTema] = useState<Tema>(
+        {
+            id: 0,
+            assunto: '',
+            descricao: '',
+            postagem: null
+        })
+    
+    const [user, setUser] = useState<User>(
+        {
+            id: 0,
+            nome: '',
+            usuario: '',
+            senha: '',
+            foto: ''
+        })
+    
+    const [postagem, setPostagem] = useState<Postagem>({
+        id: 0,
+        titulo: '',
+        texto: '',
+        tema: null,
+        usuario: null
+    })
+
+    useEffect(() => { 
+        setPostagem({
+            ...postagem,
+            tema: tema
+        })
+    }, [tema])
+
+    useEffect(() => { 
+        setPostagem({
+            ...postagem,
+            usuario: user
+        })
+    }, [user])
+
+    useEffect(() => {
+        getTemas()
+        if (id !== undefined) {
+            findByIdPostagem(id)
+        }
+    }, [id])
+
+    async function getTemas() {
+        await busca("/tema", setTemas, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    useEffect(() => {
+        getUsers()
+        if (id !== undefined) {
+            findByIdPostagem(id)
+        }
+    }, [id])
+
+    async function getUsers() {
+        await busca("/usuarios", setUsers, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+
+    async function findByIdPostagem(id: string) {
+        await buscaId(`/postagens/${id}`, setPostagem, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    function updatedPostagem(e: ChangeEvent<HTMLInputElement>) {
+
+        setPostagem({
+            ...postagem,
+            [e.target.name]: e.target.value,
+            tema: tema,
+            usuario: user
+        })
+    }
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        if (id !== undefined) {
+            put(`/postagens`, postagem, setPostagem, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+            toast.success('Postagem atualizada com sucesso', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
+        } else {
+            post(`/postagens`, postagem, setPostagem, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+            toast.success('Postagem cadastrada com sucesso', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
+        }
+        back()
+
+    }
+
+    function back() {
+        navigate('/posts')
+    }
+
+    return (
+        <>
+<Grid className='centralizarImg produto-container'>
+            <img src='https://cdn.discordapp.com/attachments/1011758147494498377/1055504651795054712/sale-removebg-preview.png'></img>
+
+            </Grid>
+        
+        
+        <Container maxWidth="sm" className="topo produto-container">
+            <form onSubmit={onSubmit}>
+                {/* <Typography variant="h3" color="textSecondary" component="h1" align="center" >Formulário de cadastro postagem</Typography> */}
+                <TextField value={postagem.titulo} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedPostagem(e)} id="titulo" label="TITULO" variant="outlined" name="titulo" margin="normal" fullWidth />
+                <TextField value={postagem.texto} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedPostagem(e)} id="texto" label="TEXTO" name="texto" variant="outlined" margin="normal" fullWidth />
+
+                <FormControl >
+                    <InputLabel id="demo-simple-select-helper-label">Tema </InputLabel>
+                    <Select
+                        labelId="demo-simple-select-helper-label"
+                        id="demo-simple-select-helper"
+                        onChange={(e) => buscaId(`/tema/${e.target.value}`, setTema, {
+                            headers: {
+                                'Authorization': token
+                            }
+                        })}>
+                        {
+                            temas.map(tema => (
+                                <MenuItem value={tema.id}>{tema.assunto}</MenuItem>
+                            ))
+                        }
+                    </Select>
+                    <FormControl>
+                    <InputLabel id="demo-simple-select-helper-label">Usuário </InputLabel>
+                    <Select
+                        labelId="demo-simple-select-helper-label"
+                        id="demo-simple-select-helper"
+                        onChange={(e) => buscaId(`/usuarios/${e.target.value}`, setUser, {
+                            headers: {
+                                'Authorization': token
+                            }
+                        })}>
+                        {
+                            users.map(user => (
+                                <MenuItem value={user.id}>{user.nome}</MenuItem>
+                            ))
+                        }
+                    </Select>
+                    </FormControl>
+                    <FormHelperText>Escolha um tema e usuário para finalizar</FormHelperText>
+                    <Button type="submit" variant="contained" color="primary" className='botaofinalizarpost imagem3'>
+                        Finalizar
+                    </Button>
+                </FormControl>
+            </form>
+        </Container>
+
+
+        </>
+    )
+
+}
+
+export default CadastroPost;
